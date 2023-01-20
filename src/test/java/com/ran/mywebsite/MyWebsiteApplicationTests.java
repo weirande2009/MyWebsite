03809506b632530a03a853f5fbd0c71a3a3aa108
weirande2009 <1209382452@qq.com>
2023-01-19 23:59:53 -0500

    Initial commit

---
+++ src/test/java/com/ran/mywebsite/MyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ran.mywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
